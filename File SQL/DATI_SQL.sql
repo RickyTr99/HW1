42dac6c07c7e71f471258ec8ec7b8d2ee492a21d
Torre Riccardo <81468651+RickyTr99@users.noreply.github.com>
2021-05-24 23:59:50 +0200

    Add files via upload

---
+++ File SQL/DATI_SQL.sql
@@ -0,0 +1,9 @@
+INSERT INTO NEWS(titolo,url,descrizione) VALUE ('GIANMARCO TAMBERI GUARITO','https://raw.githubusercontent.com/RickyTr99/MWH1/main/Tamberi.jpg', "Gianmarco Tamberi torna in pista dopo il contagio da COVID-19 avvenuto a Tourun.");
+INSERT INTO NEWS(titolo,url,descrizione) VALUE ('JACOBS DOMINA TOURUN','https://github.com/RickyTr99/MWH1/blob/main/Jacobs.jpg?raw=true', "Lamont Marcell Jacobs guadagna la medaglia d'oro nei 60m agli europei di Tourun.");
+INSERT INTO NEWS(titolo,url,descrizione) VALUE ('TORTU 10:18 PRIMA USCITA','https://github.com/RickyTr99/images/blob/main/LP_9841361-640x350.jpg?raw=true', "Filippo Tortu ferma il cronometro a 10:18, un discreto inizio di stagione");
+INSERT INTO NEWS(titolo,url,descrizione) VALUE ('NUOVO RECORD ITALIANO 100M','https://github.com/RickyTr99/images/blob/main/44628b9b1c2cc6e6f99a5d4d7fbf8747_1200x675.jpg?raw=true', "Lamont Marcell Jacobs con uno straordinario 9:95 scrive la storia della velocità italiana");
+
+
+INSERT INTO SPECIALITA(titolo,url,descrizione) VALUE ('Salti', 'https://github.com/RickyTr99/MWH2/blob/images/salto%20in%20lungo.jpg?raw=true', 'Tutte le specialità che riguardano i salti');
+INSERT INTO SPECIALITA(titolo,url,descrizione) VALUE ('Ostacoli', 'https://github.com/RickyTr99/MWH2/blob/images/ostacoli.jpg?raw=true', 'Tutte le specialità che riguardano gi ostacoli');
+INSERT INTO SPECIALITA(titolo,url,descrizione) VALUE ('Pista', 'https://github.com/RickyTr99/MWH2/blob/images/corsa%20piana.jpg?raw=true', 'Tutte le specialità della corsa su pista');
